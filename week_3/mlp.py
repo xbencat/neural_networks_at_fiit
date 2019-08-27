a02c08273f5752d5556f1f9412bfe19545c4e8a8
matus-pikuliak <matus.pikuliak@stuba.sk>
2019-08-27 15:31:00 +0200

    Week_3 WIP

---
+++ week_3/mlp.py
@@ -0,0 +1,27 @@
+
+
+class MLP:
+
+    def __init__(self, ):
+        ...
+
+    def activation(self):
+        ...
+
+    def d_activation(self):
+        ...
+
+    def loss_function(self):
+        ...
+
+    def predict(self):
+        ...
+
+    def sgd(self):
+        ...
+
+    def step(self):
+        ...
+
+    def compute_gradients(self):
+        ...
